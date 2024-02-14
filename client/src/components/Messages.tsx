a2b2fae7c44e099c4f087f3c2935547996a54dab
Shobhnik13 <shobhnikw@gmail.com>
2024-02-14 16:11:37 +0530

    Revert "completed chat ui and functionality"
    This reverts commit 647ff19fe8cd96737e12fba93be4917ee464287d.

--- client/src/components/Messages.tsx
+++ client/src/components/Messages.tsx
@@ -2,22 +2,14 @@
 
 import { useState } from "react"
 import { cn } from "../lib/utils"
-import { format } from 'date-fns'
-import Image from "next/image"
 
 interface messageProps{
   initialMessages:Message[],
   sessionId:string,
-  sessionImg:string | null | undefined
-  chatFriend: User,
 }
 
-const Messages = ({initialMessages,sessionId,sessionImg,chatFriend}:messageProps) => {
+const Messages = ({initialMessages,sessionId}:messageProps) => {
   const [messages,setMessages]=useState<Message[]>(initialMessages)
-
-  const formatTimestamp=(timestamp:number)=>{
-    return format(timestamp,'HH:mm')
-  }
   return (
     <div id="messages" className="flex h-full flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto">
       {messages.map((message,index)=>{
@@ -33,33 +25,14 @@ const Messages = ({initialMessages,sessionId,sessionImg,chatFriend}:messageProps
           return <div key={`${message.id}-${message.timestamp}`}>
                     <div className={cn('flex items-end',{'justify-end':sentByCurrentUser})}>
                         <div className={cn('flex flex-col space-y-2 text-base max-w-xs mx-2',{'order-1 items-end':sentByCurrentUser, 'order-2 items-start':!sentByCurrentUser})}>
-                          
                           <span className={cn('px-4 py-2 rounded-lg inline-block',{
                             'bg-indigo-600 text-white':sentByCurrentUser,
                             'bg-gray-200 text-gray-900':!sentByCurrentUser 
                           })}>
                             {message.text}{' '}
-                            <span className="ml-2 text-xs text-gray-400">{formatTimestamp(message.timestamp)}</span>
+                            <span className="ml-2 text-xs text-gray-400">{message.timestamp}</span>
                           </span>
                         </div>
-
-                        <div
-                        className={cn('relative w-6 h-6', {
-                        'order-2': sentByCurrentUser,
-                        'order-1': !sentByCurrentUser,
-                        invisible: hasNextMessageFromSameUser,
-                      })}>
-                      <Image
-                      fill
-                      src={
-                          sentByCurrentUser ? (sessionImg as string) : chatFriend.image
-                        }
-                      alt='Profile picture'
-                      referrerPolicy='no-referrer'
-                      className='rounded-full'
-                      />
-                    </div>  
-
                     </div>
                 </div>
            
