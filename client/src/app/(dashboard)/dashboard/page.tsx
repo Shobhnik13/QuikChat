6c2667de9fbc83da051a8405157ac9e57b6f1918
Shobhnik13 <shobhnikw@gmail.com>
2024-02-20 17:28:42 +0530

    started dashboard page backend

--- client/src/app/(dashboard)/dashboard/page.tsx
+++ client/src/app/(dashboard)/dashboard/page.tsx
@@ -1,12 +1,29 @@
+import { getFriendsByUserId } from '@/src/helper/GetFriendsByUserId'
+import { fetchRedis } from '@/src/helper/redis'
 import { authOptions } from '@/src/lib/auth'
+import { chatLinkConstructr } from '@/src/lib/utils'
 import { getServerSession } from 'next-auth'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
 const Page = async() => {
-  const sesion=await getServerSession(authOptions) 
+  const session=await getServerSession(authOptions) 
+  if(!session) return notFound()  
+
+  const friends=await getFriendsByUserId(session.user.id)
+  const friendsWithRecentMessage=await Promise.all(
+    friends.map(async(friend)=>{
+      const [lastMessage] = await fetchRedis('zrange',`chat:${chatLinkConstructr(session.user.id,friend.id)}:messages`,-1,-1) as Message[]
+    return {
+      ...friend,
+      lastMessage
+    }
+    })
+  )
+  // console.log('friendsWithRecentMessage',friendsWithRecentMessage)
   return (
     <div>
-      dashboard
+        hi
     </div>
   )
 }
