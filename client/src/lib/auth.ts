2d4eca869c2899c9e084a7e154a4701a0207f791
Shobhnik13 <shobhnikw@gmail.com>
2024-02-24 04:05:57 +0530

    changes

--- client/src/lib/auth.ts
+++ client/src/lib/auth.ts
@@ -86,7 +86,7 @@ export const authOptions:NextAuthOptions={
         //   whenever the user successfully signed in
         // we need to redirect them 
         redirect(){
-            return 'https://quik-chat-nine.vercel.app/dashboard'
+            return 'ch/dashboard'
         }
     },
     secret:'5TM/lGhe9/zDHkz4vOqp0nOBYamTF3uW5/4APS9KspE=', 
