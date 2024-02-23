28b61a5ce3f43042f54e5cb2a497b35cab35aa3f
Shobhnik13 <shobhnikw@gmail.com>
2024-02-24 03:43:22 +0530

    test

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,7 +2,6 @@ import { db } from '@/src/lib/db'
 import Image from 'next/image'
 
 export default async function Home() {
-  await db.set('elvis-bhai','sishtam')
   
   return (
     <div className='text-red-500 text-5xl'>
