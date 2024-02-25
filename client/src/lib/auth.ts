a7083cd4ed584b0abbcbb8f8c4874ea5f8c91285
Shobhnik13 <shobhnikw@gmail.com>
2024-02-25 08:35:05 +0530

    changes

--- client/src/lib/auth.ts
+++ client/src/lib/auth.ts
@@ -68,7 +68,7 @@ export const authOptions: NextAuthOptions = {
       return session
     },
     redirect() {
-      return '/dashboard'
+      return 'https://quik-chat-nine.vercel.app/dashboard'
     },
   },
 }
